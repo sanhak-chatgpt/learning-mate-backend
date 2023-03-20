d234f21b643abe7a78ac680cd94def030091f72e
Youngmin Koo <youngminz.kr@gmail.com>
2023-03-20 21:03:30 +0900

    Initial commit

---
+++ src/main/kotlin/kr/ac/kau/learningmate/LearningmateApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kau.learningmate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningmateApplication
+
+fun main(args: Array<String>) {
+	runApplication<LearningmateApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kau/learningmate/LearningmateApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kau.learningmate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearningmateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
