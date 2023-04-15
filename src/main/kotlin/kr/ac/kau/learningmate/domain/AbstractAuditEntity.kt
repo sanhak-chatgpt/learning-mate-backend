c7d988ae29b64f5ac7c62b3f9f731700c6dd30db
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2023-04-15 21:42:01 +0900

    [feat] LM-18 AbstractAuditEntity 생성

--- src/main/kotlin/kr/ac/kau/learningmate/domain/AbstractAuditEntity.kt
+++ src/main/kotlin/kr/ac/kau/learningmate/domain/AbstractAuditEntity.kt
@@ -1,5 +1,3 @@
-package com.example.umc3_teamproject.domain.item
-
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
