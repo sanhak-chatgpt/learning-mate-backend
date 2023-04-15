6ea00aacecdc062aba8027057ff23e167c3b1b7a
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2023-04-15 22:58:19 +0900

    [feat] #LM-18 AbstractAuditEntity 생성

--- src/main/kotlin/kr/ac/kau/learningmate/domain/AbstractAuditEntity.kt
+++ src/main/kotlin/kr/ac/kau/learningmate/domain/AbstractAuditEntity.kt
@@ -1,19 +1,4 @@
-import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.annotation.LastModifiedDate
-import org.springframework.data.jpa.domain.support.AuditingEntityListener
-import org.springframework.format.annotation.DateTimeFormat
-import java.time.LocalDateTime
-import javax.persistence.EntityListeners
-import javax.persistence.MappedSuperclass
+package kr.ac.kau.learningmate.domain
 
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener::class)
 abstract class AbstractAuditEntity {
-    @CreatedDate
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    var createdDate: LocalDateTime? = null
-
-    @LastModifiedDate
-    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    var modifiedDate: LocalDateTime? = null
 }
