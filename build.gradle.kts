91b13b1fbe5475140ce1878253f3c229c3939e71
choikt <choikt123456@gmail.com>
2023-04-18 00:07:46 +0900

    LM-42 main merge

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,8 +32,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation ("io.springfox:springfox-swagger-ui:3.0.0")
-	implementation ("io.springfox:springfox-swagger2:3.0.0")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-mysql")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
--- src/main/kotlin/kr/ac/kau/learningmate/service/UserService.kt
+++ src/main/kotlin/kr/ac/kau/learningmate/service/UserService.kt
@@ -1,23 +1,21 @@
 package kr.ac.kau.learningmate.service
 
 import kr.ac.kau.learningmate.domain.User
-//import kr.ac.kau.learningmate.repository.UserRepository
+import kr.ac.kau.learningmate.repository.UserRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
 @Service
 class UserService(
-    //private val userRepository: UserRepository,
+    private val userRepository: UserRepository,
 ) {
 
     fun findById(id: Long): User {
         return User(
             id = 1,
             name = "구영민",
-            createdAt = LocalDateTime.now(),
-            updatedAt = LocalDateTime.now(),
         )
-//        return userRepository.findByIdOrNull(id)!!
+        return userRepository.findByIdOrNull(id)!!
     }
 }
