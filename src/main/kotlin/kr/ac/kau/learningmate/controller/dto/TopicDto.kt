c5955a748fa20908e581beb3f57412bd1b257a2c
sumin-Heo <102477741+sumin-Heo@users.noreply.github.com>
2023-04-29 15:59:23 +0900

    [feat] 학습 선택 API #LM-25

--- src/main/kotlin/kr/ac/kau/learningmate/controller/dto/TopicDto.kt
+++ src/main/kotlin/kr/ac/kau/learningmate/controller/dto/TopicDto.kt
@@ -1,27 +1,9 @@
 package kr.ac.kau.learningmate.controller.dto
 
-class TopicController {
-    data class TopicRequestDto(
-        val subjectId: Long,
-        val page: Int,
-        val size: Int
-    )
-
-    data class TopicResponseDto(
-        val content: List<TopicDto>,
-        val pagination: PaginationDto
-    )
-
-    data class TopicDto(
+sealed class TopicDto {
+    data class Response(
         val id: Long,
         val topicName: String,
         val description: String
-    )
-
-    data class PaginationDto(
-        val page: Int,
-        val size: Int,
-        val totalElements: Long,
-        val totalPages: Int
     )
 }
