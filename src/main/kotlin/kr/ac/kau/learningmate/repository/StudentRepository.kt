ba91f2d249e0b8e7c0a435afa1b867a6eabe27b4
Youngmin Koo <youngminz.kr@gmail.com>
2023-04-30 10:51:36 +0900

    chore: 테스트용 Student API 삭제

--- src/main/kotlin/kr/ac/kau/learningmate/repository/StudentRepository.kt
+++
@@ -1,6 +0,0 @@
-package kr.ac.kau.learningmate.repository
-
-import kr.ac.kau.learningmate.domain.Student
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface StudentRepository : JpaRepository<Student, Long>
