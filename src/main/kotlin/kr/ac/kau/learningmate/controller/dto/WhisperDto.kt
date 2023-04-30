eccdc3e71fde42a4e2bcbe0f5ae8fbaa796624d5
sumin-Heo <102477741+sumin-Heo@users.noreply.github.com>
2023-04-30 17:30:12 +0900

    LM-63 Whisper 연동

---
+++ src/main/kotlin/kr/ac/kau/learningmate/controller/dto/WhisperDto.kt
@@ -0,0 +1,13 @@
+package kr.ac.kau.learningmate.controller.dto
+
+sealed class WhisperDto {
+
+    class Request(
+        val model: String,
+        val file: ByteArray,
+    )
+
+    data class Response(
+        val data: String,
+    )
+}
