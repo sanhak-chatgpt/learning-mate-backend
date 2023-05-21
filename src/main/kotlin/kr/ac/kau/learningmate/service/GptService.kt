70aeac1eebb1e6beb0cf423c7e5ec3141b27e756
choikt <choikt123456@gmail.com>
2023-05-21 15:26:47 +0900

    LM-39-3 프롬프트 수정

--- src/main/kotlin/kr/ac/kau/learningmate/service/GptService.kt
+++ src/main/kotlin/kr/ac/kau/learningmate/service/GptService.kt
@@ -25,13 +25,19 @@ class GptService(
     fun completeChat(prompt: String): String {
         val input = GptDto.Request(
             model = "gpt-4",
-            temperature = 0.9,
+            temperature = 1.15,
             messages = listOf(
+                GptDto.Request.Message(
+                    role = "user",
+                    content = """
+                         아래의 형식으로 데이터를 JSON 으로 만들어줘. 설명하지마. 마크다운으로 주지 마. 오로지 JSON 형태로만 줘.
+                         {"score":"/* 0~100사이의 강의에 대한 점수 정확도를 기준으로 측정*/","strength":"/* 200자 내외의 내용의 정확도를 좋았던 점*/","weakness":"/* 200자 내외의 정확도를 기준으로 추가해야 할 내용*/"}
+                    """.trimIndent()
+                ),
                 GptDto.Request.Message(
                     role = "user",
                     content = prompt,
                 )
-
             ),
         )
 
